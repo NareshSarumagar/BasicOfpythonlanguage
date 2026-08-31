d554f1452699b926e9468e25ceaa382d9ae31743
NareshSarumagar <xxxttaction@gmail.com>
2026-08-31 22:44:33 +0545

    Code modified of ps.py

--- Phase-1/Problemsolving/ps.py
+++ Phase-1/Problemsolving/ps.py
@@ -4,16 +4,16 @@
 # divisible by both, otherwise print the number.
 # Uses: Loop, if-elif-else, % operator.
 
-# for i in range(1,101):
-#     if i%3 == 0:
-#         print("Fizz")
+for i in range(1,101):
+    if i%3 == 0:
+        print("Fizz")
 
-#     if i%5 == 0:
-#         print("Buzz")
+    if i%5 == 0:
+        print("Buzz")
 
-#     if i%3 and i%5 == 0:
-#         print("FizzBuzz")
-#     print(i)
+    if i%3 and i%5 == 0:
+        print("FizzBuzz")
+    print(i)
 
 #     Find the sum and average of a list without using sum() or statistics.mean().
 # Uses: Lists, Loop, Variables.
