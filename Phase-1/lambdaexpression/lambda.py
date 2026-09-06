ec546e0a0fc88f0c4dd46655be8db8cb227de368
NareshSarumagar <xxxttaction@gmail.com>
2026-09-06 15:40:47 +0545

    code change of lambda.py

---
+++ Phase-1/lambdaexpression/lambda.py
@@ -0,0 +1,15 @@
+#normal funtion -> lambda is a one line function
+
+def function(x):
+    return x**2
+
+function(3)
+
+#lamda expression 
+
+add = lambda x: x**2
+
+add(4)
+
+commitment = lambda :"i will commit"
+commitment()
